58fe6c04fa316455717f8a67acf55025262dc90c
FelicitasKlix <felikpc@gmail.com>
2024-05-17 16:44:30 -0300

    first code box

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,12 @@ import streamlit as st
 import streamlit_authenticator as stauth
 
 st.title("Script Repository")
+
+code = """
+def hello():
+    print("Hello, Streamlit!")
+
+hello()
+"""
+
+st.code(code, language='python')
